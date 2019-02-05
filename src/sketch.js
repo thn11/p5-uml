2d469dd1f1b0d113f898e95f97f7d692f982cbd1
Thomas Naesje <thomas@scorchsoft.com>
2019-02-05 17:16:49 +0000

    Added some mouse control

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,44 @@
 let obj;
+let view;
+let oldMousePos;
+
+// TODO: fix mouse paralax
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     obj = new Node(createVector(width/2, height/2));
+    view = {
+        zoom: 1,
+        pos: createVector(0,0),
+    };
+    oldMousePos = createVector(mouseX, mouseY);
 }
 
 function draw() {
+    push();
+    translate(width/2, height/2);
+    scale(view.zoom);
+    translate(view.pos.x - width/2, view.pos.y - height/2);
     background(255);
+    if (mouseIsPressed){
+        view.pos.sub(oldMousePos.sub(createVector(mouseX, mouseY)));
+        oldMousePos = createVector(mouseX, mouseY);
+    }
     obj.tick();
     obj.show();
-    obj.setPos(createVector(mouseX, mouseY));
+    pop();
+}
+
+function mousePressed(e) {
+    oldMousePos = createVector(mouseX, mouseY);
+}
+
+function mouseWheel(e) {
+    if (e.delta < 0){
+        view.zoom += view.zoom / 10;
+    } else {
+        view.zoom -= view.zoom / 10;
+    }
 }
 
 
