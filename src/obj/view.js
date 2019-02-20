1c6e59e6e9aff553391d4b269b1a8c38d8945e05
tkon <thomas@naesje.com>
2019-02-20 23:31:44 +0000

    Trying to fix zoom

--- src/obj/view.js
+++ src/obj/view.js
@@ -4,18 +4,18 @@ class View {
     this.width = 0;
     this.height = 0;
     this.zoomLevel = 1;
-    this.realMousePos = createVector(0,0);
-    this.worldMouse = createVector(0,0);
-    this.mousePos = createVector(0,0);
+    this.realMousePos = createVector(0, 0);
+    this.worldMouse = createVector(0, 0);
+    this.mousePos = createVector(0, 0);
     this.dragEnabled = true;
   }
 
 
   anchorMouse() {
-      this.mousePos = createVector(
-          mouseX / this.zoom,
-          mouseY / this.zoom
-      );
+    this.mousePos = createVector(
+      mouseX / this.zoom,
+      mouseY / this.zoom
+    );
   }
 
   get zoom() {
@@ -27,42 +27,43 @@ class View {
    * delta (int) - negative number for zoom out, positive for zoom in
    */
   set zoom(delta) {
-      const oldZoom = this.zoomLevel;
-      if (delta !== undefined){
-          this.zoomLevel += this.zoomLevel / (50 / delta);
-      }
-      const zoomDelta = this.zoomLevel - oldZoom;
-      this.pos.sub(createVector(
-          mouseX * zoomDelta,
-          mouseY * zoomDelta
-      ))
-      console.log(this.pos);
+    const oldZoom = this.zoomLevel;
+    if (delta !== undefined) {
+      this.zoomLevel += this.zoomLevel / (50 / delta);
+    }
+    let scaleDelta = this.zoomLevel - oldZoom;
+    let zoomDelta = createVector(
+      -((width * scaleDelta)),
+      -((height * scaleDelta))
+    )
+    //https://stackoverflow.com/questions/2916081/zoom-in-on-a-point-using-scale-and-translate
+    this.pos = this.pos.add(zoomDelta);
   }
 
   tick() {
-      this.worldMouse = createVector(
-          mouseX/view.zoom - view.pos.x,
-          mouseY/view.zoom - view.pos.y
+    this.worldMouse = createVector(
+      mouseX / view.zoom - view.pos.x,
+      mouseY / view.zoom - view.pos.y
+    );
+
+    this.height = height / this.zoom;
+    this.width = width / this.zoom;
+
+
+    if (mouseIsPressed && this.dragEnabled) {
+      this.realMousePos = createVector(
+        mouseX / this.zoom,
+        mouseY / this.zoom
       );
-
-      this.height = height / this.zoom;
-      this.width = width / this.zoom;
-
-
-      if (mouseIsPressed && this.dragEnabled){
-          this.realMousePos = createVector(
-              mouseX / this.zoom,
-              mouseY / this.zoom
-          );
-          this.pos = this.pos.sub(this.mousePos.sub(this.realMousePos));
-          this.mousePos = this.realMousePos.copy();
+      this.pos = this.pos.sub(this.mousePos.sub(this.realMousePos));
+      this.mousePos = this.realMousePos.copy();
 
 
 
 
 
 
-      }
+    }
 
 
     return null;
